fe4ba16d52dc80a3da5dc84e33a435a83fa3087c
vladislav <akulllvlllad@gmail.com>
2022-09-15 16:50:38 +0700

    add link to BD

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ import { UserController, PostController } from './controllers/index.js'
 
 
 
-//mongodb+srv://admin:wwwwww@cluster0.jgttjos.mongodb.net/app?retryWrites=true&w=majority
+//
 mongoose
 	.connect(
-		process.env.MONGODB_URI 	
+		process.env.MONGODB_URI || 'mongodb+srv://admin:wwwwww@cluster0.jgttjos.mongodb.net/app?retryWrites=true&w=majority'
 	)
 	.then(() => console.log('DB ok'))
 	.catch(err => console.log('DB error', err))
